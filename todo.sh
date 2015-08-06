6012e1ad551f1fbadc1d666caa8c3e8486236ad5
Haoran Qi <haoransky@gmail.com>
2015-08-05 23:06:58 -0400

    Modified todo.sh

--- todo.sh
+++ todo.sh
@@ -8,37 +8,31 @@
 # Support : Linux
 # Requirements : bash
 # Version: 1.0
-# Last Updated: August 5, 2015 by Esaad 
+# Last Updated: August 5, 2015 by Esaad
 #####################################################################
 
 
-# make sure you use todo -GA 
+# make sure you use todo -GA
+set -x
 
-echo "Script ran at `date`";
+echo "Script ran at $(date)";
 
 
-# check if todo file exists 
-todo_path="$HOME/.todo.txt";
-backup_path="$HOME/.todo.txt.tmp";
-
-# small check to create backup path if user forgets to create
-if [ ! -d "$backup_path" ]; then
-    # Control will enter here if $backup_path doesn't exist.
-    echo "**** TODO FILE DOES NOT EXIST ****"
-    echo "**** CREATING TODO FILE: $backup_path ****"
-    echo `todo -GA` > $backup_path;
-    chmod 777 $backup_path; 
-fi
+# check if todo file exists
+result_path="/tmp/todo"
+backup_path="/tmp/todo.tmp"
 
 # check to see if todo has changed or not
+echo "$(todo -G --sort done,created)" | tee $backup_path
 
-echo "`todo -GA`" > $todo_path;
+diff $result_path $backup_path
+rval=$?
 
-DIFF=$(diff $todo_path $backup_path) 
-if [ "$DIFF" != "" ] 
+if [[ $rval -ne 0 ]]
 then
-    echo "The directory was modified"
-    notify-send -u critical -t 1 -i appointment-soon.png "***REMINDER***" "`cat $todo_path`"
+    cp $backup_path $result_path
+    notify-send -u critical -i appointment-soon.png "***REMINDER***" "\n$(cat $result_path)"
 fi
+rm -f $backup_path
 
 echo "End of script";
